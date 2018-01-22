2bf373b53f308dacd18b4d24deb970ae7a23cd03
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-22 21:32:09 +0100

    preuzimanje zapisa iz baze, izlistavanje svih zapisa

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,9 @@
+<h1>Todos</h1>
+@if(count($todos) > 0)
+    @foreach($todos as $todo)
+        <div class="well">
+            <h3>{{$todo->text}}</h3>
+            <span class="label label-danger">{{$todo->due}}</span>
+        </div>
+    @endforeach
+@endif
