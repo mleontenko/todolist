446247a0db7c1d32f3255056d4c4b5e98cc92eff
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-22 21:55:21 +0100

    compile assets, dodan template i view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>TodoList</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <footer class="text-center">
+            <p>Copyright &copy; 2017 TodoList</p>
+        </footer>
+    </body>
+</html>
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,9 +1,14 @@
-<h1>Todos</h1>
-@if(count($todos) > 0)
-    @foreach($todos as $todo)
-        <div class="well">
-            <h3>{{$todo->text}}</h3>
-            <span class="label label-danger">{{$todo->due}}</span>
-        </div>
-    @endforeach
-@endif
+@extends('layouts.app')
+
+
+@section('content')
+    <h1>Todos</h1>
+    @if(count($todos) > 0)
+        @foreach($todos as $todo)
+            <div class="well">
+                <h3>{{$todo->text}}<span class="label label-danger">{{$todo->due}}</span></h3>
+                
+            </div>
+        @endforeach
+    @endif
+@endsection
