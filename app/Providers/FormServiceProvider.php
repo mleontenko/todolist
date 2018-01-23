27a20ed8f646c0cf3bf91e308ee5c181f9dec961
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-23 15:24:59 +0100

    Edit & Update

--- app/Providers/FormServiceProvider.php
+++ app/Providers/FormServiceProvider.php
@@ -16,7 +16,8 @@ class FormServiceProvider extends ServiceProvider
     {
         Form::component('bsText', 'components.form.text', ['name', 'value' => null, 'attributes' => []]);
         Form::component('bsTextArea', 'components.form.textArea', ['name', 'value' => null, 'attributes' => []]);
-        Form::component('bsSubmit', 'components.form.submit', ['value' => 'Submit', 'attributes' => []]);        
+        Form::component('bsSubmit', 'components.form.submit', ['value' => 'Submit', 'attributes' => []]);
+        Form::component('hidden', 'components.form.hidden', ['name', 'value' => null, 'attributes' => []]);        
     }
 
     /**
